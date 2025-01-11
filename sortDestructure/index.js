7d5029cfe942567a3818d7cdae939a1059c7f7e7
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-01-11 14:20:44 +0200

    Implement solution

--- sortDestructure/index.js
+++ sortDestructure/index.js
@@ -1,1 +1,7 @@
-# 
+const people = ['Bernard, Sandra', 'Bethea, Erl'. 'Becker, Carl', 'Bent, Silas'];
+
+const alpha = people.sort((a, b) => {
+    const [aLast, aFirst] = people.split(', ');
+    const [bLast, bFirst] = people.split(', ');
+    return aLast > bLast ? 1 : -1;  // compare last names alphabetically
+}); 
